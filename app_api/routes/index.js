9c5d539f51b646cdf838f67769df4118e38e5797
Baileya93 <alyssa.bailey1@snhu.edu>
2024-08-18 23:33:40 -0400

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -8,10 +8,12 @@ const tripsController = require('../controllers/trips');
 router
     .route('trips')
     .get(tripsController.tripsList);
+    .post(tripsController.tripsAddTrip);
 
 // get method routes tripsFindByCode - reqires parameter
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode);
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
